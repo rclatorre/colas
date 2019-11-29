9c84866f281e47a632fe1d3e0bb537df2b07b340
Roberto Carlos Latorre Letona <rclatorre@gmail.com>
2019-11-29 10:47:52 -0500

    make it better

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,26 @@
+//Comando para establecer la conexion
+
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('desconectado del servidor');
+});
+
+socket.on('estadoActual', function(resp) {
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguiente) {
+
+        label.text(siguiente);
+    });
+
+});
---
+++ server/sockets/socket.js
@@ -0,0 +1,44 @@
+const { io } = require('../server');
+const { TicketControl } = require('../classes/ticket-control');
+
+const ticketControl = new TicketControl(); //  <----- con esto disparamos el constructor 
+
+io.on('connection', (client) => {
+
+    // Escuchar el cliente
+    client.on('siguienteTicket', (data, callback) => {
+
+        let siguiente = ticketControl.siguiente();
+
+        console.log(siguiente);
+
+        callback(siguiente);
+    });
+
+    client.emit('estadoActual', {
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    });
+    
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        console.log('Antes de broadcast ultimos4');
+
+        client.broadcast.emit('ultimos4', {
+            ultimos4: ticketControl.getUltimos4()
+        });
+
+    });
+
+
+});
